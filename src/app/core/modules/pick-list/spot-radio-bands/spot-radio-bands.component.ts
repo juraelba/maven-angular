765b46250114809b1f03f7f48790b18ca2b163d1
juraelba <trepechovdimitar@gmail.com>
2022-09-12 17:22:47 +0300

    Added the No data found page

--- src/app/core/modules/pick-list/spot-radio-bands/spot-radio-bands.component.ts
+++ src/app/core/modules/pick-list/spot-radio-bands/spot-radio-bands.component.ts
@@ -3,11 +3,15 @@ import { filter, map, takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
 import { ListsService } from '@services/lists/lists.service';
+import { SelectedCriteriaService } from '@services/selected-criteria/selected-criteria.service';
+import { SearchService } from '@services/search/search.service';
+
 import { SelectOption } from '@models/select.model';
 import { SelectedCriteriaEvent } from '@models/criteries.model';
 import { ListChangesEvent } from '@models/list.model';
+
 import { ListKeys } from '@enums/lists.enum';
-import { SelectedCriteriaService } from '@services/selected-criteria/selected-criteria.service';
+import { SearchActionTypesEnum } from '@enums/search.enum';
 
 @Component({
   selector: 'app-spot-radio-bands',
@@ -25,7 +29,8 @@ export class SpotRadioBandsComponent implements OnInit {
 
   constructor(
     private listsService: ListsService,
-    private selectedCriteriaService: SelectedCriteriaService
+    private selectedCriteriaService: SelectedCriteriaService,
+    private searchService: SearchService
   ) { }
 
   ngOnInit(): void {
@@ -42,6 +47,27 @@ export class SpotRadioBandsComponent implements OnInit {
         this.change.emit({ key: ListKeys.radiobands, data: selected });
       });
 
+    this.listenSelectedCriteriaChanges();
+    this.listenSearchBarMenuActions();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll.next(null);
+    this.unsubscribeAll.complete();
+  }
+
+  listenSearchBarMenuActions(): void {
+    this.searchService.searchBarEvents$
+      .pipe(
+        takeUntil(this.unsubscribeAll),
+        filter(({ action }) => SearchActionTypesEnum.NEW_SEARCH === action)
+      )
+      .subscribe(() => {
+        this.onClear();
+      });
+  }
+
+  listenSelectedCriteriaChanges(): void {
     this.selectedCriteriaService.selectedCriteria$
       .pipe(
         takeUntil(this.unsubscribeAll),
@@ -55,11 +81,6 @@ export class SpotRadioBandsComponent implements OnInit {
         this.borderLabel = this.listsService.getBorderLabel(options, ListKeys.radiobands);
         this.options = updatedOptions;
       });
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribeAll.next(null);
-    this.unsubscribeAll.complete();
   }
 
   onApplyChanges(options: SelectOption[]): void {
@@ -82,5 +103,4 @@ export class SpotRadioBandsComponent implements OnInit {
 
     this.change.emit({ key: ListKeys.radiobands, data: [] });
   }
-
 }
