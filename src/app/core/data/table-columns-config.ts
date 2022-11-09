a530ca83457975e522dbe5acddc257f536c92e2e
juraelba <trepechovdimitar@gmail.com>
2022-11-09 15:09:44 +0200

    Matched fields order according to documentation

--- src/app/core/data/table-columns-config.ts
+++ src/app/core/data/table-columns-config.ts
@@ -8,8 +8,8 @@ import { SearchQuery } from '@models/search.model';
 import { TableColumnsConfig } from '@models/table.model';
 
 const NETWORK_COLUMNS = [
-  { id: SearchColumnsIdEnum.networks, label: SearchColumnsEnum.networks },
   { id: SearchColumnsIdEnum.mavenid, label: SearchColumnsEnum.mavenid },
+  { id: SearchColumnsIdEnum.networks, label: SearchColumnsEnum.networks },
   { id: SearchColumnsIdEnum.subType, label: SearchColumnsEnum.subType },
   { id: SearchColumnsIdEnum.owner, label: SearchColumnsEnum.owner },
   { id: SearchColumnsIdEnum.parent, label: SearchColumnsEnum.parent },
@@ -101,8 +101,17 @@ export const TABLE_COLUMNS: TableColumnsConfig = {
   [SearchEnum['network-radio']]: NETWORK_COLUMNS,
   [SearchEnum.digital]: DEFAULT_COLUMNS,
   [SearchEnum.magazine]: [
-    ...DEFAULT_COLUMNS,
+    { id: SearchColumnsIdEnum.name, label: SearchColumnsEnum.name },
+    { id: SearchColumnsIdEnum.mavenid, label: SearchColumnsEnum.mavenid },
+    { id: SearchColumnsIdEnum.type, label: SearchColumnsEnum.type },
+    { id: SearchColumnsIdEnum.subType, label: SearchColumnsEnum.subType },
     { id: SearchColumnsIdEnum.market, label: SearchColumnsEnum.market },
+    { id: SearchColumnsIdEnum.owner, label: SearchColumnsEnum.owner },
+    { id: SearchColumnsIdEnum.parent, label: SearchColumnsEnum.parent },
+    { id: SearchColumnsIdEnum.slogan, label: SearchColumnsEnum.slogan },
+    { id: SearchColumnsIdEnum.categories, label: SearchColumnsEnum.categories },
+    { id: SearchColumnsIdEnum.website, label: SearchColumnsEnum.website },
+    { id: SearchColumnsIdEnum.partners, label: SearchColumnsEnum.partners },
     { id: SearchColumnsIdEnum.fullPage, label: SearchColumnsEnum.fullPage },
     {
       id: SearchColumnsIdEnum.circulation,
@@ -110,8 +119,17 @@ export const TABLE_COLUMNS: TableColumnsConfig = {
     },
   ],
   [SearchEnum.newspaper]: [
-    ...DEFAULT_COLUMNS,
+    { id: SearchColumnsIdEnum.name, label: SearchColumnsEnum.name },
+    { id: SearchColumnsIdEnum.mavenid, label: SearchColumnsEnum.mavenid },
+    { id: SearchColumnsIdEnum.type, label: SearchColumnsEnum.type },
+    { id: SearchColumnsIdEnum.subType, label: SearchColumnsEnum.subType },
     { id: SearchColumnsIdEnum.market, label: SearchColumnsEnum.market },
+    { id: SearchColumnsIdEnum.owner, label: SearchColumnsEnum.owner },
+    { id: SearchColumnsIdEnum.parent, label: SearchColumnsEnum.parent },
+    { id: SearchColumnsIdEnum.slogan, label: SearchColumnsEnum.slogan },
+    { id: SearchColumnsIdEnum.categories, label: SearchColumnsEnum.categories },
+    { id: SearchColumnsIdEnum.website, label: SearchColumnsEnum.website },
+    { id: SearchColumnsIdEnum.partners, label: SearchColumnsEnum.partners },
     {
       id: SearchColumnsIdEnum.dailyFullPageRate,
       label: SearchColumnsEnum.dailyFullPageRate,
@@ -121,13 +139,34 @@ export const TABLE_COLUMNS: TableColumnsConfig = {
       label: SearchColumnsEnum.dailyCirculation,
     },
   ],
-  [SearchEnum.outdoor]: DEFAULT_COLUMNS,
+  [SearchEnum.outdoor]: [
+    { id: SearchColumnsIdEnum.name, label: SearchColumnsEnum.name },
+    { id: SearchColumnsIdEnum.mavenid, label: SearchColumnsEnum.mavenid },
+    { id: SearchColumnsIdEnum.subType, label: SearchColumnsEnum.subType },
+    { id: SearchColumnsIdEnum.market, label: SearchColumnsEnum.market },
+    { id: SearchColumnsIdEnum.owner, label: SearchColumnsEnum.owner },
+    { id: SearchColumnsIdEnum.parent, label: SearchColumnsEnum.parent },
+    { id: SearchColumnsIdEnum.slogan, label: SearchColumnsEnum.slogan },
+    { id: SearchColumnsIdEnum.categories, label: SearchColumnsEnum.categories },
+    { id: SearchColumnsIdEnum.website, label: SearchColumnsEnum.website },
+  ],
   [SearchEnum.diverse]: DEFAULT_COLUMNS,
   [SearchEnum.callHistory]: [
     { id: SearchColumnsIdEnum.changeOn, label: SearchColumnsEnum.changeOn },
     { id: SearchColumnsIdEnum.changeFrom, label: SearchColumnsEnum.changeFrom },
     { id: SearchColumnsIdEnum.changeTo, label: SearchColumnsEnum.changeTo },
     { id: SearchColumnsIdEnum.change, label: SearchColumnsEnum.change },
+  ],
+
+  [SearchEnum['network-radio']]: [
+    { id: SearchColumnsIdEnum.mavenid, label: SearchColumnsEnum.mavenid },
+    { id: SearchColumnsIdEnum.networks, label: SearchColumnsEnum.networks },
+    { id: SearchColumnsIdEnum.subType, label: SearchColumnsEnum.subType },
+    { id: SearchColumnsIdEnum.owner, label: SearchColumnsEnum.owner },
+    { id: SearchColumnsIdEnum.parent, label: SearchColumnsEnum.parent },
+    { id: SearchColumnsIdEnum.categories, label: SearchColumnsEnum.categories },
+    { id: SearchColumnsIdEnum.website, label: SearchColumnsEnum.website },
+    { id: SearchColumnsIdEnum.partners, label: SearchColumnsEnum.partners },
   ],
 };
 
